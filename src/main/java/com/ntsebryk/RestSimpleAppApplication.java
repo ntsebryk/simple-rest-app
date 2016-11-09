ae94158603673130d94e46e5c5cf0651d799aeb3
ntsebryk <ntsebryk@softserveinc.com>
2016-11-09 15:30:01 +0200

    Initial Commit

---
+++ src/main/java/com/ntsebryk/RestSimpleAppApplication.java
@@ -0,0 +1,12 @@
+package com.ntsebryk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSimpleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSimpleAppApplication.class, args);
+	}
+}
